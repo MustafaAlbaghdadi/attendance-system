c8dcfe36aefcb3f2a6329006cf3bb13dc685d94f
MustafaAlbaghdadi <redarrowd@gmail.com>
2021-10-23 09:59:33 +0300

    inital project

---
+++ precache-manifest.b973353da54cc037f0be9c3c6325c610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba28c219bcd2fbba87964e4ac8a9d82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b36d8246858e1fcc72",
+    "url": "/static/css/main.8b0eb5b7.chunk.css"
+  },
+  {
+    "revision": "ec77076916908a754542",
+    "url": "/static/js/2.c985b469.chunk.js"
+  },
+  {
+    "revision": "22b36d8246858e1fcc72",
+    "url": "/static/js/main.8f7814b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
