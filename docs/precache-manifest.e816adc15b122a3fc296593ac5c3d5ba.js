08a34b916f025a3e78ef414db30caf3be16581dc
MustafaAlbaghdadi <redarrowd@gmail.com>
2021-10-23 11:15:36 +0300

    change build to docs

---
+++ docs/precache-manifest.e816adc15b122a3fc296593ac5c3d5ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7eebbc5ef50153c7edb0a2ff239e2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e2d10d5e1c397b6565",
+    "url": "/static/css/main.8b0eb5b7.chunk.css"
+  },
+  {
+    "revision": "83b1199768a29957767f",
+    "url": "/static/js/2.2a1e9cb1.chunk.js"
+  },
+  {
+    "revision": "e5e2d10d5e1c397b6565",
+    "url": "/static/js/main.763c0645.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
